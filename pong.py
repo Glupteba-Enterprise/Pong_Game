1e73f3c24ce24526ca72768b381ab9e459635b14
scaffpine <scaffpine2@gmail.com>
2023-03-01 10:56:54 +1300

    added key press detector

--- pong.py
+++ pong.py
@@ -1,16 +1,17 @@
 """pong gmaing"""
 import tkinter as tk
+from tkinter import *
 
 # defining variables
 gui_title = "Glupteba Pong"
-gui_label = "The Big Glup"
+gui_text = "The Big Glup"
 gui_width = 500
 gui_height = 500
 
 # creating gui instance
 gui_main = tk.Tk()
 
-### getting the screen center
+"""getting the screen center"""
 
 # getting screen dimensions
 screen_width = gui_main.winfo_screenwidth()
@@ -19,11 +20,22 @@ screen_height = gui_main.winfo_screenheight()
 gui_x = int(screen_width / 2 - gui_width / 2)
 gui_y = int(screen_height / 2 - gui_height / 2)
 
+
+
 # setting gui variables
 gui_main.title(gui_title)
 gui_main.geometry(f'{gui_width}x{gui_height}+{gui_x}+{gui_y}')  # width x height ± x ± y
-_gui_label = tk.Label(gui_main, text=gui_label)
-_gui_label.pack()
 
+# key press detector. will be used for 'press any key to start game'
+def onKeyPress(event):
+    """sets the 'on key press' text"""
+    gui_text_var.insert('end', f'You pressed {(event.char, )}\n')
+
+# shows text on screen
+gui_text_var = tk.Text(gui_main, background='black', foreground='white', font=('Comic Sans MS', 12))
+gui_text_var.pack()
+
+# listens for any 'KeyPress' event
+gui_main.bind('<KeyPress>', onKeyPress)
 # run the gui
 gui_main.mainloop()
